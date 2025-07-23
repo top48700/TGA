6fc45c6fb54f550bf6a2220a58fdcef094b527a1
top48700 <wangcaifeng58@gmail.com>
2025-07-23 11:29:38 +0700

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
+import TGA from '../../public/TG-logo.png'
+import '../components/css/Header.css'
+
 function Header() {
     return (
-        <>
-        
-        </>
+        <div className="header">
+            <div className="logo">
+                <span>
+                    <img src={TGA} width={220} height={120} />
+                </span>
+            </div>
+            <div className="nav-items">
+                <nav>
+                    <ul className='nav-links'>
+                        <li className='bg'>Home</li>
+                        <li className='bg'>About Us</li>
+                        <li className='bg'>Service</li>
+                        <li className='bg'>Content</li>
+                        <li className='bg'>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     )
 } export default Header;
